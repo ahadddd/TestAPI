c61ce375d12adaa3d5f066db26b76848846597d9
ahadddd <ahad.shehbaz19@gmail.com>
2023-06-13 16:19:39 +0500

    Add project files.

---
+++ TestAPI/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestAPI.Data;
+using TestAPI.Models;
+
+namespace TestAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController: Controller 
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Get()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateUser([FromBody] User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserByID(int id)
+        {
+            var user = await _context.Users.Where(user => user.Id == id).FirstOrDefaultAsync();
+            if (user != null)
+                return user;
+            return NotFound();
+        }
+    }
+}
